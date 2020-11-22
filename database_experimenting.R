4c100792b3763a88c2aa74fa3201551678e976d6
Roshni Srikanth <rsrika@uw.edu>
2020-11-22 11:29:58 -0800

    Combined imdb_500 and imdb_movies into one dataframe using the specific columns we want

--- database_experimenting.R
+++ database_experimenting.R
@@ -1,5 +1,5 @@
 library(dplyr)
-
+library(stringr)
 #Roshni's experimental data analysis
 imdb_5000_movies_df <- read.csv("data/imdb_5000_movie_dataset.csv", stringsAsFactors = FALSE)
 imdb_movies_df <- read.csv("data/IMDb_movies.csv", stringsAsFactors = FALSE)
@@ -7,19 +7,28 @@ movies_lionsgate_df <- read.csv("data/movies_lionsgate.csv", stringsAsFactors = 
 movies_metadata_df <- read.csv("data/movies_metadata.csv", stringsAsFactors = FALSE)
 # We want to combine the dataframes, using the movie titles as the common factor 
 # First, we need to make sure that all the movie titles are called the same thing -> title
-imdb_5000_movies_df <- mutate(imdb_5000_movies_df, "title" = substr(movie_title, 1, nchar(movie_title)-1))
+imdb_5000_movies_df <- mutate(imdb_5000_movies_df, "title" = substr(movie_title, 1, nchar(movie_title)-2))
 movies_lionsgate_df <- mutate(movies_lionsgate_df, "title" = name)
+
+imdb_movies_df <- imdb_movies_df %>%
+                  select(title, genre, usa_gross_income,
+                         budget, language, country, avg_vote,
+                         reviews_from_critics)
+
+imdb_5000_movies_df <- imdb_5000_movies_df %>%
+                       select(title, plot_keywords)
+
 
 
 # These two datasets have 2584 observations in common
-movie_big1_df <- left_join(imdb_5000_movies_df, imdb_movies_df, by = "title" )
+movie_big1_df <- inner_join(imdb_5000_movies_df, imdb_movies_df, by = "title" )
 
 # These two datasets have 7493 movies in common, which might be useful
-movie_big2_df <- left_join(movies_lionsgate_df, movies_metadata_df, by = "title" )
+# movie_big2_df <- left_join(movies_lionsgate_df, movies_metadata_df, by = "title" )
 
 # These three databases have 4996 movies in common, but many of them are repeats
-movie_big3_df <- inner_join(movie_big1_df, movie_big2_df, by = "title")
+# movie_big3_df <- inner_join(movie_big1_df, movie_big2_df, by = "title")
 
 
 # Looking at the number of unique movie titles, gives us around 3192 movies to work with
-n_unique_titles <- length(unique(movie_big3_df$title))
+n_unique_titles <- length(unique(movie_big1_df$title))
