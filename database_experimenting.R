a6135731fc676befc23f469dfb038d4b4b3e9f0a
emirin1 <emirin1@uw.edu>
2020-11-22 11:28:19 -0800

    Fixed substring issue

--- database_experimenting.R
+++ database_experimenting.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(stringr)
 
 #Roshni's experimental data analysis
 imdb_5000_movies_df <- read.csv("data/imdb_5000_movie_dataset.csv", stringsAsFactors = FALSE)
@@ -7,9 +8,8 @@ movies_lionsgate_df <- read.csv("data/movies_lionsgate.csv", stringsAsFactors = 
 movies_metadata_df <- read.csv("data/movies_metadata.csv", stringsAsFactors = FALSE)
 # We want to combine the dataframes, using the movie titles as the common factor 
 # First, we need to make sure that all the movie titles are called the same thing -> title
-imdb_5000_movies_df <- mutate(imdb_5000_movies_df, "title" = substr(movie_title, 1, nchar(movie_title)-1))
+imdb_5000_movies_df <- mutate(imdb_5000_movies_df, "title" = str_sub(movie_title, 1, nchar(movie_title)-2))
 movies_lionsgate_df <- mutate(movies_lionsgate_df, "title" = name)
-
 
 # These two datasets have 2584 observations in common
 movie_big1_df <- left_join(imdb_5000_movies_df, imdb_movies_df, by = "title" )
