7283c18ae166bbb3ebf61ace2e30fcb7e297fe7d
Ha To <nhatha.to.2001@gmail.com>
2020-11-30 14:23:25 -0800

    In summary_info: change the column names into normal english

--- scripts/summary_info.R
+++ scripts/summary_info.R
@@ -1,26 +1,41 @@
-# Summary Information Script
+# Summary information for the data set
 
 # To delete all variables in my work space for testing later
 rm(list = ls())
 
 # Load libraries
 library(lintr)
+library(stringr)
 
 # A function  that takes in a dataset and returns a list of info about it:
 get_summary_info <- function(dataset) {
   ret <- list()
+
+  # Getting number of rows and columns in the dataset
   ret$number_of_obs <- nrow(dataset)
   ret$number_of_columns <- ncol(dataset)
-  ret$column_names <- paste(colnames(dataset)[-1], sep = "", collapse = ", ")
-  #
+
+  # Getting the names of the variables in normal English
+  col_name_eng <- lapply(colnames(dataset), function(x) {
+    gsub("_", " ", x)
+    })
+  col_name_eng <- lapply(col_name_eng, str_to_title)
+  ret$column_names <- paste(col_name_eng[-1], sep = "", collapse = ", ")
+
+  # Getting the earliest year in the time frame
   earliest_year_vect <- dataset %>%
     filter(year == min(year)) %>%
     pull(year) #pull gives a vector with all occurrences of the earliest year
   ret$earliest_year <- earliest_year_vect[1]
-  #
+
+  # Get the latest year in the time frame
   latest_year_vect <- dataset %>%
     filter(year == max(year)) %>%
     pull(year)
   ret$latest_year <- latest_year_vect[1]
   return(ret)
 }
+
+# Test
+midpoint <- read.csv("midpoint_df.csv", stringsAsFactors = F)
+get_summary_info(midpoint)
