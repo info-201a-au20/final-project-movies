7234e93667ad5a5d84c9357a5a0dd348beb5549b
Roshni Srikanth <rsrika@uw.edu>
2020-12-02 14:09:57 -0800

    added comment sections

--- app_ui.R
+++ app_ui.R
@@ -1,2 +1,14 @@
 # UI
+# First Page (introduction)
 
+
+# Second Page (interactive chart 1)
+
+
+# Third Page (interactive chart 2)
+
+
+# Fourth Page (interactive chart 3)
+
+
+# Fifth Page (summary)
