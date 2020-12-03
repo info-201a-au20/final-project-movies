72e120cf3a99796db7f0fc636cd0c52a56ccf79a
emirin1 <emirin1@uw.edu>
2020-12-03 11:42:16 -0800

    Experimented with checkbox input

--- pages/scatter_page.R
+++ pages/scatter_page.R
@@ -1,1 +1,19 @@
 # Scatter Plot page
+
+# List of genres
+# Get data
+data <- read.csv("./data/midpoint_df.csv", stringsAsFactors = F)
+# Filter, split multiple genres into individual genres and find unique genres
+genre_list <- data %>%
+  select(genre) %>%
+  separate_rows(genre, sep = ", ") %>%
+  unique() %>%
+  as.list()
+
+# checkboxGroupInput for selecting genres
+genre_checkbox <- checkboxGroupInput(
+  inputId = "genre_selection",
+  label = "Genre",
+  choices = genre_list,
+  selected = genre_list
+)
