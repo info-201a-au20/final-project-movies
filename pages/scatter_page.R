a577be949e7faabff65a62d75c03ec026fcecd94
Melina Perraut <melina@perraut.us>
2020-12-07 13:20:49 -0800

    Changed tabPanel title and formatting for scatterplot

--- pages/scatter_page.R
+++ pages/scatter_page.R
@@ -26,7 +26,9 @@ scatter_main_content <- mainPanel(
 # Creating a `tabPanel()` for the scatterplot page
 scatter_page <- tabPanel(
   "Genre Popularity",
-  titlePanel("Popularity of Movie Genres From 1990-2020"),
+  includeCSS("styling.css"),
+  titlePanel(
+    tags$h1("Popularity of Movie Genres From 1990-2020")),
   sidebarLayout(
     scatter_sidebar_content,
     scatter_main_content
