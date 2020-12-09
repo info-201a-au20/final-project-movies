4c4878c4f394606bd84a898ddfa2187c46aa6772
Ha To <nhatha.to.2001@gmail.com>
2020-12-08 18:44:59 -0800

    Intro page: Update limitations; add footer image

--- pages/overview_page.R
+++ pages/overview_page.R
@@ -6,14 +6,17 @@ intro_page <- tabPanel(
     # Image
     tags$img(id = "cropped_image",
              src = "movietheater.png", alt = "Image of Movie Theater"),
+
     h1("About This Project"),
     includeHTML("overview_page.html"),
-    
-      bsCollapsePanel(
-        "Click to learn more about the limitation of our project",
-        includeHTML("intro_limitations.html")
-      ),
-    
+
+    bsCollapsePanel(
+      "Click to learn more about the limitations of our project",
+      includeHTML("intro_limitations.html")
+    ),
+
+    tags$img(id = "cropped_footer_image",
+             src = "movietheater.png", alt = "Image of Movie Theater"),
     
     tags$footer(tags$p("Gisele Fox, Emiri Nishizawa, Melina Perraut, Roshni Srikanth, Ha Nhat To: ", 
                        tags$a(id = "url", href= "https://github.com/info-201a-au20/final-project-movies", "Github URL"))
